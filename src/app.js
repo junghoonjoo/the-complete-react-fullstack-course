643042da8aaf7fe1201f986da153e6a94355eae7
junghoonjoo <junghoonjoo@gmail.com>
2023-01-23 17:11:20 +0900

    useState with object 배열이나 object형태의 state를 사용하는 방법 예시

--- src/app.js
+++ src/app.js
@@ -2,27 +2,62 @@ import React,{useState} from 'react';
 
 const App=(props)=> {
 
-    const [count,setCount]=useState(props.initialCount);
+    const [state,setState]=useState({
+        count:props.initialCount,
+        user:"Francis"
+    });
 
-    console.log(count);
+    let [posts, setPosts]=useState([
+        {
+            name:"Super awesome hooks",
+            body:"Everything you need for your hook"
+        }
+    ]);
+
+    //console.log(state);
 
     const restOne=()=>{
-        setCount(prevCount=>{
-            return prevCount-1;
+        setState(prevState=>{
+            return {...prevState,count:prevState.count-1};
         });
     }
 
-    const reset=()=>{
-        setCount(props.initialCount);
-    }
+    // const reset=()=>{
+    //     setCount(props.initialCount);
+    // }
 
+    const addOnePost=()=>{
+        let newPost={
+            name:"Sugar is bad",
+            body:"coffe is good"
+        }
+
+        setPosts([
+            ...posts,newPost
+        ]);
+    }
   return (
-    <div>
-        <h3>Count: {count}</h3>
-        <button onClick={()=>{setCount(count+1)}}>Add one +1</button>
-        <button onClick={restOne}>Minus one -1</button>
-        <button onClick={reset}>Reset</button>
-    </div>
+    <>
+        <h1>{state.user}</h1>
+        <h3>Count: {state.count}</h3>
+        <button onClick={()=>{setState({...state,count:state.count+1})}}>Add one +1</button>
+        <button onClick={restOne}>Rest One -1</button>
+        <button onClick={()=>setState({
+            ...state,count:props.initialCount
+        })}>Reset</button>
+
+        <hr/>
+
+        {posts.map((item,i)=>(
+            <div key={i}>
+                <div>Name:{item.name}</div>            
+                <div>Body:{item.body}</div>            
+                <hr/>
+            </div>            
+        ))}
+        <button onClick={addOnePost}>Add Post</button>
+
+    </>
   );
 }
 
