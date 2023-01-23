89c6395cd9779bcce21bd00ed898924cae1f9c5a
junghoonjoo <junghoonjoo@gmail.com>
2023-01-23 16:43:28 +0900

    simple example of useState, to understand how to

--- src/app.js
+++ src/app.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React,{useState} from 'react';
 
-function App() {
+const App=(props)=> {
+
+    const [count,setCount]=useState(props.initialCount);
+
+    console.log(count);
+
+    const restOne=()=>{
+        setCount(prevCount=>{
+            return prevCount-1;
+        });
+    }
+
+    const reset=()=>{
+        setCount(props.initialCount);
+    }
+
   return (
     <div>
-        <h3>Count: 0</h3>
+        <h3>Count: {count}</h3>
+        <button onClick={()=>{setCount(count+1)}}>Add one +1</button>
+        <button onClick={restOne}>Minus one -1</button>
+        <button onClick={reset}>Reset</button>
     </div>
   );
 }
