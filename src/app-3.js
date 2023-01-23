378ba2386f2545a5570a83bbb3a09c178dc609af
junghoonjoo <junghoonjoo@gmail.com>
2023-01-23 20:10:55 +0900

    class 방식을 활용한 useRef의 이해
    class방식으로 dom에 직접 접근하기
    위한 예제, useRef를 설명하기 위함

---
+++ src/app-3.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+    constructor(props){
+        super(props)
+
+        this.textInput=React.createRef();
+    }
+
+  triggerHandler = ()=>{
+    console.log(this.textInput.current.value);
+  }
+  render() {
+    return (
+      <>
+        <h1>Form: </h1>
+        <input type='text' ref={this.textInput}/>
+        <button onClick={this.triggerHandler}>Trigger</button>
+      </>
+    );
+  }
+}
